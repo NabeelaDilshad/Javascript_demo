fc685d21a8c447323fc8a76e85415c36fd7d16ea
faizuddin53 <faizuddinansari53@gmail.com>
2024-03-24 12:41:59 +0530

    find max in array

--- basic.js
+++ basic.js
@@ -10,4 +10,19 @@ function minimum() {
     return min;
 }
 const r = minimum(array)
-console.log(r)
+console.log('min  number is ',r)
+
+
+// find  max  in the array
+const array1 = [10, 20, 50, 70, 3, 80]
+function findMax() {
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >  max) {
+            max= array[i]
+        }
+    }
+    return max;
+}
+const maxresult = findMax(array)
+console.log('max number is ', maxresult)
